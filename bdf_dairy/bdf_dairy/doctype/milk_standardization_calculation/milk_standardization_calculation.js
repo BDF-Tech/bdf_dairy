342cffc378da7e86d838582832d10a264085258f
sanprasoftwares <sanprasoftwares@gmail.com>
2026-01-03 13:21:04 +0530

    Update on Milk Stand

--- bdf_dairy/bdf_dairy/doctype/milk_standardization_calculation/milk_standardization_calculation.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, BDF and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Milk Standardization Calculation", {
-// 	refresh(frm) {
-
-// 	},
-// });
