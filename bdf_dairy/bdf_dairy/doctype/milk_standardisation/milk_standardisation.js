f0010b01c55da129a23b7f017d0570665e7accb5
Sanskar199 <sanskar19shukla@gmail.com>
2026-09-21 17:26:44 +0530

    chore: remove old Milk Standardisation from bdf_dairy
    Milk Standardisation now lives in the dairy app (milk_entry module,
    feature/milk-standardisation). Keeping the old copy here would overwrite
    the dairy DocType on migrate, since bdf_dairy syncs after dairy.
    
    Removes:
    - Milk Standardisation, Milk Standardisation Silo Details,
      Milk Standardisation Silo Rank
    - Milk Standardization (old doctype)
    - Milk Standardization Consumption Report

--- bdf_dairy/bdf_dairy/doctype/milk_standardisation/milk_standardisation.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, BDF and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Milk Standardisation", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- bdf_dairy/bdf_dairy/doctype/milk_standardization/test_milk_standardization.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, BDF and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestMilkStandardization(FrappeTestCase):
-	pass
